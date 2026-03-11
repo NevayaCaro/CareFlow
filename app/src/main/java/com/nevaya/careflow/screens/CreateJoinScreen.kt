a4b5508e72636190d5534bf2c6562b151e170020
Nevaya <nevayacaro@gmail.com>
2026-03-11 13:03:39 -0400

    Edits fo create/join

--- app/src/main/java/com/nevaya/careflow/screens/CreateJoinScreen.kt
+++ app/src/main/java/com/nevaya/careflow/screens/CreateJoinScreen.kt
@@ -9,7 +9,7 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun CreateJoinScreen(modifier: Modifier = Modifier) {
-
+// Creates a screen function with an optional modifier
     Column(
         modifier = modifier
             .fillMaxSize()
@@ -17,21 +17,24 @@ fun CreateJoinScreen(modifier: Modifier = Modifier) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-
+         //makes items vertical, added 24dp space, makes
+        // column fill the screen, and centers item horizontal/vertical
         Text(
             text = "CareFlow",
             style = MaterialTheme.typography.headlineLarge
         )
-
+//displays the text careflow on the screen large
         Spacer(modifier = Modifier.height(40.dp))
-
+//adds 40dp space between these two
         Button(
             onClick = { /* TODO: Navigate to Create Shift */ },
             modifier = Modifier.fillMaxWidth()
         ) {
             Text("Create Shift")
         }
-
+//creates filled button does not work to
+// click on fully yet, stretched text, and
+// shows text "Create Shift"
         Spacer(modifier = Modifier.height(16.dp))
 
         OutlinedButton(
@@ -39,6 +42,6 @@ fun CreateJoinScreen(modifier: Modifier = Modifier) {
             modifier = Modifier.fillMaxWidth()
         ) {
             Text("Join Shift")
-        }
+        }//made another bottom does same thing as above
     }
 }
