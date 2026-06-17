cfc0986599bc66f849c212fdf3730451c1152902
Nevaya <nevayacaro@gmail.com>
2026-06-17 10:26:12 -0400

    switched nav to splash

--- app/src/main/java/com/nevaya/careflow/navigation/AppNavGraph.kt
+++ app/src/main/java/com/nevaya/careflow/navigation/AppNavGraph.kt
@@ -15,7 +15,7 @@ fun AppNavGraph(navController: NavHostController, padding: PaddingValues, startD
 
     NavHost(
         navController = navController,
-        startDestination = "home"
+        startDestination = "splash"
     ) {
 
         // SPLASH
